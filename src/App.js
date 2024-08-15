cd24707522d1081c44e631c0e90f6edaf314e1da
Gorst0902 <dinhvanthanhan0902@gmail.com>
2024-08-15 11:01:03 +0700

    create components Cards and Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Analytics from "./components/Analytics";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Newsletter from "./components/Newsletter";
@@ -11,6 +13,8 @@ function App() {
       <Hero />
       <Analytics />
       <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   );
 }
